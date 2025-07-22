565abb42b77718e2312bcef87966110747c34a16
msaad973 <muhammadsaadullah973@gmail.com>
2025-07-22 17:35:00 +0500

    importsidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -6,6 +6,7 @@ import '@fontsource/roboto/700.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/NavBar';
 import ProductList from './Components/ProductList';
+import Sidebar from './Components/SideBar';
 import { Provider } from 'react-redux';
 import store from './store/store';
 
@@ -13,13 +14,18 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-        </Routes>
+        <div style={{ display: 'flex' }}>
+          <Sidebar />
+          <div style={{ flexGrow: 1 }}>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<ProductList />} />
+            </Routes>
+          </div>
+        </div>
       </Router>
     </Provider>
   );
 }
 
-export default App
+export default App;
